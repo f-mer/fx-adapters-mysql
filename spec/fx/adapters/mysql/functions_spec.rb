ca737c3c5eed009492f22ca16fd62a19402521a8
slava pocheptsov <pocheptsov@gmail.com>
2022-10-14 03:21:07 +0000

    remove database_url database config param

--- spec/fx/adapters/mysql/functions_spec.rb
+++ spec/fx/adapters/mysql/functions_spec.rb
@@ -10,16 +10,37 @@ RSpec.describe Fx::Adapters::MySQL::Functions do
         RETURN 'test';
       EOS
 
+      connection.execute <<-EOS.strip_heredoc
+        CREATE FUNCTION test2 (input_param INT)
+        RETURNS INT DETERMINISTIC
+        BEGIN
+          RETURN input_param*2;
+        END;
+      EOS
+
       functions = described_class.new(connection).all
 
-      first = functions.first
-      expect(functions.size).to eq 1
-      expect(first.name).to eq "test"
-      expect(first.definition).to eq <<-EOS.strip_heredoc.chomp
+      test_function = functions.first
+      expect(functions.size).to eq 2
+      expect(test_function.name).to eq "test"
+      expect(test_function.definition).to eq <<-EOS.strip_heredoc.chomp
         DELIMITER $$
-        CREATE FUNCTION `test`() RETURNS text CHARSET #{ActiveRecord::Base.connection.charset}
+        CREATE FUNCTION `test`() RETURNS text CHARSET #{connection.charset}
             DETERMINISTIC
         RETURN 'test'$$
+        DELIMITER ;
+      EOS
+
+      test2_function = functions.last
+      expect(functions.size).to eq 2
+      expect(test2_function.name).to eq "test2"
+      expect(test2_function.definition).to eq <<-EOS.strip_heredoc.chomp
+        DELIMITER $$
+        CREATE FUNCTION `test2`(input_param INT) RETURNS int(11)
+            DETERMINISTIC
+        BEGIN
+          RETURN input_param*2;
+        END$$
         DELIMITER ;
       EOS
     end
